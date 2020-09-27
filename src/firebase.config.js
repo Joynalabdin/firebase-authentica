b90bd2e317072282faa5015137915427d3a3dafa
Md Joynal Abdin Robel <robel.joynal@gmail.com>
2020-09-27 10:46:35 +0600

    start google authentication

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDlzx3tHUZyq1aaUke5sV_CDoc0w31upg",
+    authDomain: "simple-two-78c45.firebaseapp.com",
+    databaseURL: "https://simple-two-78c45.firebaseio.com",
+    projectId: "simple-two-78c45",
+    storageBucket: "simple-two-78c45.appspot.com",
+    messagingSenderId: "1074803755023",
+    appId: "1:1074803755023:web:db462d57de4cc19e873b7a"
+  };
+  export default firebaseConfig;
